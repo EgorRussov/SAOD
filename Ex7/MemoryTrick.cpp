eedd409d8d5aa6162b5a32de9fe90ee8eb5f9663
EgorRussov <124882933+EgorRussov@users.noreply.github.com>
2023-04-13 12:42:54 +0300

    Add files via upload

---
+++ Ex7/MemoryTrick.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include "Test.h"
+
+using namespace std;
+
+shared_ptr<Test> foo() {
+    shared_ptr<Test> ptr(new Test());
+    ptr->Val = 10; 
+    return ptr;
+}
+
+Test* goo() {
+    Test* ptr = new Test();
+    ptr->Val = 10;
+    return ptr;
+}
+
+int main()
+{
+    vector<int> v;
+    int a = v.capacity();
+    for (int i = 0; i < 32; i++) {
+        v.push_back(1);
+        int b = v.capacity();
+        if (b != a) {
+            cout << v.size() << " " << v.capacity() << endl;
+            a = v.capacity();
+        }
+    }
+    shared_ptr<Test> t;
+    {
+        auto p = foo();
+        t = p;
+        cout << p->Val << endl;
+    } 
+    cout << "after block" << endl;
+    auto g = goo();
+    cout << "Val = " << g->Val << endl;
+    delete g;
+}
